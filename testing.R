d68320b1b0364f573f3d88ab5d5ccf8fc57b7c53
witchergill <anoopsingh247365@gmail.com>
2024-12-24 12:20:13 +0400

    added some loop and old dataset in the mainframe

--- testing.R
+++ testing.R
@@ -3,3 +3,12 @@
 print("hello world")
 Sys.sleep(5)
 print("this is my first commit")
+
+#creating a new for loop for the work
+
+for(i in 1:10){
+print(i*i*i)
+Sys.sleep(1)
+}
+
+read_csv('old_data.csv')
