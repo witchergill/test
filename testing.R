711bbc4f16ed60fae10fc88663dc4222df7f5363
witchergill <anoopsingh247365@gmail.com>
2024-12-24 12:24:11 +0400

    defined minor finishing

--- testing.R
+++ testing.R
@@ -1,14 +1,15 @@
 #testing 
 
 print("hello world")
-Sys.sleep(5)
+Sys.sleep(2)
 print("this is my first commit")
 
 #creating a new for loop for the work
 
 for(i in 1:10){
 print(i*i*i)
-Sys.sleep(1)
+Sys.sleep(.1)
 }
 
-read_csv('old_data.csv')
+head(read_csv('old_data.csv'))
+Sys.sleep(5)
